bae1e53b75f8ede5cfd5a7320d0bb3a4d23bf50a
Thibaud Colas <thibaudcolas@gmail.com>
2016-02-06 23:29:30 +0200

    Clean up JSdom rendering code

--- jsdom.js
+++ jsdom.js
@@ -2,19 +2,20 @@ var fs = require('fs');
 var jsdom = require('jsdom');
 var saveSvgAsPng = require('save-svg-as-png');
 var dataUriToBuffer = require('data-uri-to-buffer');
-// var svg2png = require('svg2png');
 
 var chart = require('./chart');
 
 jsdom.env({
     html: '<html><head><link rel="stylesheet" href="http://localhost:1337/chart.css" /></head><body><svg class="chart"></svg></body></html>',
     features: {
-        FetchExternalResources: ['link'],
+        FetchExternalResources: ['link', 'img'],
         ProcessExternalResources: true,
     },
     done: function(err, window) {
         global.window = window;
         global.document = window.document;
+        global.Canvas = require('canvas');
+        global.Image = Canvas.Image;
 
         var numbers = [ 4, 8, 15, 16, 23, 42 ];
 
@@ -22,9 +23,6 @@ jsdom.env({
 
         saveSvgAsPng.svgAsDataUri(document.querySelector('.chart'), {}, function(uri) {
             var buffer = dataUriToBuffer(uri);
-
-            console.log(uri);
-            console.log(buffer.toString());
 
             fs.writeFile('test-jsdom.svg', buffer, function() {
                 console.log('wrote test-jsdom.svg');
